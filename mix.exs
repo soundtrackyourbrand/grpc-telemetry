ac8f9fd9fed79b18c074635b70c7310cc7b20903
Fredrik Enestad <fredrik@enestad.com>
2019-12-16 16:16:16 +0100

    version bump

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GRPCTelemetry.MixProject do
   use Mix.Project
 
-  @version "1.0.0-rc.1"
+  @version "1.0.0-rc.2"
   @description "Telemetry interceptor/middleware for grpc"
 
   def project do
